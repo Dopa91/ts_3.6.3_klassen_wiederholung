be3521c8f27352a21874c40f191f2639a17a893b
Dopa91 <code.dopatka@gmail.com>
2024-09-04 11:12:23 +0200

    completed task hello_world.dart

--- hello_world.dart
+++ hello_world.dart
@@ -1,3 +1,16 @@
-void main () {
-  
+// Klasse, ohne Attribute
+class HelloWorld {
+
+// Methode print
+  void printMessage() {
+    print("Hello, World!");
+  }
 }
+
+// Funktion mit ausgabe der Methode: printMessage
+void main () {
+HelloWorld outputPrint = HelloWorld();
+  outputPrint.printMessage();
+}
+
+// Denkfrage: Wird nicht benötigt, es wird von der funktion "class" automatisch gemacht, wenn keine Attributte gegeben sind.
