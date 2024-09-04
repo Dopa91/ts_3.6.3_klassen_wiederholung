aadbd7586e5746ca340d6a8125bae0df78849961
Dopa91 <code.dopatka@gmail.com>
2024-09-04 17:02:21 +0200

    completed task

--- person.dart
+++ person.dart
@@ -3,8 +3,36 @@ String name;
 int age;
 String adress;
 
+Person(this.name, this.age, this.adress);
+void dataPerson() {
+  print("$name ist $age Jahre alt und wohnt hier: $adress");
+}
+
+//Methode zur Namensänderung
+  void setName(String newName) {
+    name = newName;
+  }
+  //Methode zur ALtersänderung
+  void setAge(int newAge) {
+    age = newAge;
+  }
+  //Methode zur ADressänderung
+  void setAdress(String newAdress) {
+    adress = newAdress;
+  }
 }
 
 void main() {
-  
+Person person1 = Person("Andreas", 33, "BlaStr.1, 12345 BlaTown");
+Person person2 = Person("Mina", 7, "UpsTown, 23456 UpsLand");
+print("");
+person1.dataPerson();
+print("");
+person2.dataPerson();
+
+
+person1.setAge(25);
+person1.dataPerson();
+
 }
+
